722787c628a22812bcddaa0f8cf826e80773f849
Sriram Revanth Turlapati <sturlapati@thelcfgroup.com>
2024-12-03 20:39:01 -0500

    Initial commit

---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig  {
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/lead").hasAuthority("ROLE_LEAD")
+                    .requestMatchers("/user").hasAuthority("ROLE_USER")
+                    .anyRequest().authenticated()
+            )
+            .oauth2Login();
+        return http.build();
+    }
+
+
+}
